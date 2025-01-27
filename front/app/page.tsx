c75de7e96990200ca89752832dfed61d2872f937
Philip Johnson <philip.asd@gmail.com>
2025-01-27 07:14:45 -0300

    practicando tailwind

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,32 +3,33 @@ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Tabl
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start" style={{ background: "red" }}>
-        <Table>
-          <TableCaption>A list of your recent invoices.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="text-right">$250.00</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+    <div className="h-96 w-96 bg-white">
 
 
 
-      </main>
+
 
     </div>
   );
 }
+
+
+{/* <Table>
+<TableCaption>A list of your recent invoices.</TableCaption>
+<TableHeader>
+  <TableRow>
+    <TableHead className="w-[100px]">Invoice</TableHead>
+    <TableHead>Status</TableHead>
+    <TableHead>Method</TableHead>
+    <TableHead className="text-right">Amount</TableHead>
+  </TableRow>
+</TableHeader>
+<TableBody>
+  <TableRow>
+    <TableCell className="font-medium">INV001</TableCell>
+    <TableCell>Paid</TableCell>
+    <TableCell>Credit Card</TableCell>
+    <TableCell className="text-right">$250.00</TableCell>
+  </TableRow>
+</TableBody>
+</Table> */}
