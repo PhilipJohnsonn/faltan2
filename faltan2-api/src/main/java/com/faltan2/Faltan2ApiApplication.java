0203f291cafd9d7bdda50bdc98f57065689a13e4
Philip Johnson <philip.asd@gmail.com>
2025-02-02 11:51:20 -0300

    first commit

---
+++ faltan2-api/src/main/java/com/faltan2/Faltan2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.faltan2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Faltan2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Faltan2ApiApplication.class, args);
+	}
+
+}
---
+++ faltan2-api/src/test/java/com/faltan2/Faltan2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faltan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Faltan2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
